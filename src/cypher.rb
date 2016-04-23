a0b44ff3cfb670bd5390934f6d43c94de0ccfba0
UserXGnu <userx.gnu@gmail.com>
2016-04-23 12:51:14 -0300

    Adding config_file to plugin interface

--- src/cypher.rb
+++ src/cypher.rb
@@ -7,10 +7,10 @@ Dir[File.dirname(__FILE__) + '/plugins/crypto/*.rb'].each {|file| require_relati
 
 
 class Cypher < AbstractCypher
-    attr_accessor :type
-    attr_accessor :key
+    attr_reader :config_filepath
 
-    def initialize(type, key)
+    def initialize(type, key, config_filepath = nil)
+        @config_filepath = config_filepath
         @key = key if key != nil
         set_type(type)
 
@@ -19,7 +19,7 @@ class Cypher < AbstractCypher
     private
     def set_type(type)
        begin
-          @instance = eval(type).new(@key)
+          @instance = (@config_filepath.nil?) ? eval(type).new(@key) : eval(type).new(@key, @config_filepath)
           raise if !(@instance.kind_of? AbstractCypher)
        rescue
            raise ArgumentError.new('Invalid cypher type or class doesn\'t inherit from AbstractCypher')        
