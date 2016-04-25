f39bf563e4f500bd7f76a26e48986badfec258ba
n3k00n3 <n3k00n3@protonmail.com>
2016-04-25 19:25:27 -0300

    vigenere plugin

--- main.rb
+++ main.rb
@@ -10,30 +10,30 @@ require_relative 'src/stego'
 =end
 
 # message and key to be used on encryption process
-message="ABC"
-key = 1
+message="LBMCOCJMSSDCX"
+key = "limaolimaolim"
 
 #instanciating a Cypher object based on Ceasar plugin.
-c = Cypher.new("Ceasar", key)
+v = Cypher.new("Vigenere", key)
 
 #instanciating a Stego object based on Txt plugin.
-s = Stego.new("Txt", "teste")
+#s = Stego.new("Txt", "teste")
 
 #CRYPTO
 #encrypting message
-c.encrypt(message)
+#v.encrypt(message)
 
 #STEGNO
 #hiding message
-s.hide(message)
+#s.hide(message)
 
 #unhiding message
-message = s.unhide
+#message = s.unhide
 
 #CRYPTO
 #decrypting message
-c.decrypt(message)
+v.decrypt(message)
 
+#puts message
 #printing it
 #puts "If virgulino's plugins are working right you should see \'ABC\' above:\n#{message}"
-print "#{message}"
--- src/plugins/crypto/vigenere.rb
+++ src/plugins/crypto/vigenere.rb
@@ -1,18 +1,57 @@
 require_relative '../abstract_cypher'
 
+
 class Vigenere < AbstractCypher
 
     def initialize(key)
-        @key = key
+      @key = key
     end
 
     public
     def encrypt(content)
-        puts "Callint encrypt: #{content}"
+      key = @key
+	  key.upcase!
+      key = key.each_byte.to_a
+      content.upcase!
+      content = content.each_byte.to_a
+      i = 0
+      na = 65
+      l = 26
+      while i < content.length
+        j = key[i] + content[i]
+        j = j - 65
+        char = j - 26
+        if char < 65
+          char = char + 26
+        end
+
+        content[i] = char.chr
+        i += 1
+      end
+      content = content.join
+      puts content
     end
 
-    def decrypt(content)
-        puts "Callint decrypt: #{content}"
-        content
-    end
+  def decrypt(content)
+	  key = @key
+	  key.upcase!
+      content.upcase!
+      key = key.each_byte.to_a
+      content = content.each_byte.to_a
+      i = 0
+      na = 65
+      l = 26
+      while i < content.length
+        j = content[i] - key[i]
+        j = j + 65
+        char = j + 26
+        if char > 90
+          char = char - 26
+        end
+	    content[i] = char.chr
+        i += 1
+      end
+	  content = content.join
+      puts content
+  end
 end
