c1c9ecf3dedf1a37447f0cfb8ac7b6ba1aac417b
n3k00n3 <n3k00n3@protonmail.com>
2016-04-26 11:34:31 -0300

    adding a private method

--- src/plugins/crypto/vigenere.rb
+++ src/plugins/crypto/vigenere.rb
@@ -7,24 +7,9 @@ class Vigenere < AbstractCypher
       @key = key
     end
 
-    def keyMatch(content)
-    key = @key
-    if key.length < content.length
-      i = 0
-      while key.length < content.length
-      key << key[i]
-      i += 1
-      end
-    else key.length > content.length
-      while key.length > content.length
-        key.chop!
-      end
-      $key = key
-    end
-
     def encrypt(content)
       key = keyMatch(content)
-	  key.upcase!
+      key.upcase!
       key = key.each_byte.to_a
       content.upcase!
       content = content.each_byte.to_a
@@ -36,16 +21,15 @@ class Vigenere < AbstractCypher
         if char < 65
           char = char + 26
         end
-
         content[i] = char.chr
         i += 1
       end
       content = content.join
     end
 
-  def decrypt(content)
-	  key = keyMatch(content)
-	  key.upcase!
+    def decrypt(content)
+      key = keyMatch(content)
+      key.upcase!
       content.upcase!
       key = key.each_byte.to_a
       content = content.each_byte.to_a
@@ -57,9 +41,26 @@ class Vigenere < AbstractCypher
         if char > 90
           char = char - 26
         end
-	    content[i] = char.chr
+        content[i] = char.chr
         i += 1
       end
-	  content = content.join
-  end
+      content = content.join
+    end
+
+    private
+    def keyMatch(content)
+      key = @key
+      if key.length < content.length
+        i = 0
+        while key.length < content.length
+        key << key[i]
+        i += 1
+        end
+      else key.length > content.length
+        while key.length > content.length
+          key.chop!
+        end
+        $key = key
+      end
+    end
 end
