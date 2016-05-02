d8ffe0107f62a0819f9905d0f51b6c340f78108c
UserXGnu <userx.gnu@gmail.com>
2016-05-02 19:50:49 -0300

    Getopt feature (incomplete)

---
+++ virgulino.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+require "getoptlong"
+require_relative 'src/cypher'
+require_relative 'src/stego'
+
+class Virgulino
+  def initialize
+  end
+
+  public
+  def handler
+    @opts = GetoptLong.new(
+      ["--help",        "-h", GetoptLong::NO_ARGUMENT],
+      ["--options",     "-?", GetoptLong::NO_ARGUMENT],
+      ["--encrypt",     "-e", GetoptLong::REQUIRED_ARGUMENT],
+      ["--decrypt",     "-d", GetoptLong::REQUIRED_ARGUMENT],
+      ["--key",         "-k", GetoptLong::REQUIRED_ARGUMENT],
+      ["--input"        "-i", GetoptLong::REQUIRED_ARGUMENT],
+      ["--output",      "-o", GetoptLong::REQUIRED_ARGUMENT],
+      ["--steg",        "-s", GetoptLong::REQUIRED_ARGUMENT],
+    )
+
+
+    @opts.each do |opt, arg|
+      case opt
+      when "--encrypt"
+        @encrypt = true
+        @enc_type = arg
+      when "--decrypt"
+        @decrypt = true
+        @enc_type = arg
+      when "--key"
+        @key = arg
+      when "--input"
+        @input = true
+        @input_filepath = arg
+      when "--output"
+        @output = true
+        @output_filepath = arg
+      when "-steg"
+        @steg = true
+        @steg_type = arg
+      else
+        puts "[!!] invalid option #{opt} [!!]"
+
+        self.usage
+        exit
+      end
+    end
+  end
+
+  private
+  def usage
+    puts "./virgulino [-e/-d <encryption type>] [-m <message>] [-k <key>] [-i <input filepath>] [-o <output filepath>] [-s <steg_type>]"
+  end
+
+end
+
